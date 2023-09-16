c4e8004de2f484a480d715e17ceb1f490c8f1603
Varsha <68608622+vaarshha@users.noreply.github.com>
2023-09-16 18:41:35 +0530

    CRU ops for tasks

---
+++ src/main/java/com/rescue/vscube/task/TaskController.java
@@ -0,0 +1,30 @@
+package com.rescue.vscube.task;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+    @GetMapping("")
+    public ResponseEntity<List<Task>> getAllTasks() {
+        return ResponseEntity.ok(taskService.getALLTasks());
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Task> addTask(@RequestBody Task task) {
+        return ResponseEntity.ok(task);
+    }
+
+
+
+}
