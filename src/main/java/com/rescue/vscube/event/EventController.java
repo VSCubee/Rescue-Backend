ba600b81c80b199e27c53f355bc184c66501fc35
vigbav36 <90998381+vigbav36@users.noreply.github.com>
2023-09-18 17:03:08 +0530

    fix/ cors error

--- src/main/java/com/rescue/vscube/event/EventController.java
+++ src/main/java/com/rescue/vscube/event/EventController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RequestMapping("/api/events")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin
 @RestController
 public class EventController {
 
